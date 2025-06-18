5739ae613b8ca19cafb0c787332595c28c0e2ec8
Bellic <159970540+SrBellic@users.noreply.github.com>
2025-06-18 18:53:42 -0400

    App running and first route added

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import categoryRoutes from './routes/category';
+import { connectDB } from './db/connection';
+
+const app = express();
+const PORT = 3000;
+const logger = morgan('dev');
+
+//Middlewares
+connectDB().catch((err) => {
+	console.error('Error connecting to the database:', err);
+});
+app.use(cors());
+app.use(logger);
+app.use(express.json());
+
+//Routes
+app.use('/api/', categoryRoutes);
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('¡Hola, TypeScript con Express!');
+});
+
+app.listen(PORT, () => {
+	console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
