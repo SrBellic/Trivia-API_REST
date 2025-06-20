d67183ed145ce727675868c848fe7e95b9942e2f
Bellic <159970540+SrBellic@users.noreply.github.com>
2025-06-20 16:32:23 -0400

    Add error classes

---
+++ src/classes/errors.ts
@@ -0,0 +1,18 @@
+export class BadRequestError extends Error {
+	constructor(message: string) {
+		super(message);
+		this.name = 'BadRequestError';
+	}
+}
+export class NotFoundError extends Error {
+	constructor(message: string) {
+		super(message);
+		this.name = 'NotFoundError';
+	}
+}
+export class InternalServerError extends Error {
+	constructor(message: string) {
+		super(message);
+		this.name = 'InternalServerError';
+	}
+}
